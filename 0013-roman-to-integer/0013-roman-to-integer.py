77e8a2c7adc1984599b64bff79732a4376bfd171
Sreshta Siliveri <sreshtasiliveri@gmail.com>
2025-01-25 16:02:37 +0530

    Time: 3 ms (80.88%), Space: 17.9 MB (27.11%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If this is not the last character and the next character is larger,
+            # we need to subtract the current value (this is the subtractive notation).
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
